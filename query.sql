e565a160da08f576d5c8fcf2d02df6f0200cda7c
philippe gabriel <philippegabriel@hotmail.com>
2026-09-23 11:25:05 +0100

    Cosmetic changes for the html

--- query.sql
+++ query.sql
@@ -2,9 +2,5 @@ select
     date(published_iso) as date,
     title,
     description,
-    web_url as "Web URL",
-    podcast_title as "Podcast title",
-    podcast_url as "Podcast URL",
-    player_url as "Player URL",
-    id
+    web_url as "Web URL"
 from rf_html;
